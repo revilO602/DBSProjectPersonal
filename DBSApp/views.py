15c4b402bbf56e7f50731cfd334471515d6e8c59
Oliver Leontiev <leontiev.oliver@gmail.com>
2021-02-20 12:34:06 +0100

    added raw sql to get uptime form v1/health

--- DBSApp/views.py
+++ DBSApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.db import connection
+from django.http import HttpResponse
 
-# Create your views here.
+def uptime(request):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT date_trunc('second', current_timestamp - pg_postmaster_start_time()) as uptime;")
+        row = cursor.fetchone()
+
+    return HttpResponse(row)
--- DBSProject/urls.py
+++ DBSProject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from DBSApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('v1/health/', views.uptime),
 ]
